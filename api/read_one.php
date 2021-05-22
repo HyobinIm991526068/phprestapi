6bb254cccd92101a845fb9cd45e1bf258aacbc17
ihb92 <jakeim9@gmail.com>
2021-05-22 08:26:05 -0400

    Finished read_one post

---
+++ api/read_one.php
@@ -0,0 +1,29 @@
+<?php
+
+//headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+//call the initialize.php (initializing the api)
+include_once('../core/initialize.php');
+
+//instantiate post
+$post = new Post($db);
+
+$post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+$post->read_one();
+
+$post_arr = array(
+    'id' => $post->id,
+    'title' => $post->title,
+    'body' => $post->body,
+    'author' => $post->author,
+    'category_id' => $post->category_id,
+    'category_name' => $post->category_name
+);
+
+//make into json
+print_r(json_encode($post_arr));
+
+?>
